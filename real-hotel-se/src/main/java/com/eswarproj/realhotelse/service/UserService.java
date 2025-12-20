3e2bbe9c40f35e5d2e116d37af48e32fe33baf9e
Prithvi M <prithvim1311@gmail.com>
2025-12-20 16:32:14 +0530

    updated code

--- real-hotel-se/src/main/java/com/eswarproj/realhotelse/service/UserService.java
+++ real-hotel-se/src/main/java/com/eswarproj/realhotelse/service/UserService.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 
-
-
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
@@ -27,36 +25,37 @@ public class UserService implements IUserService {
 
     @Override
     public User registerUser(User user) {
-        if (userRepository.existsByEmail(user.getEmail())){
+        if (userRepository.existsByEmail(user.getEmail())) {
             throw new UserAlreadyExistsException(user.getEmail() + " already exists");
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         System.out.println(user.getPassword());
-        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        Role userRole = roleRepository.findByName("ROLE_USER").orElseGet(() -> {
+            System.out.println("Creating missing ROLE_USER");
+            return roleRepository.save(new Role("ROLE_USER"));
+        });
         user.setRoles(Collections.singletonList(userRole));
         return userRepository.save(user);
     }
 
-@Override
-public List<User> getUsers() {
-    return userRepository.findAll();
-}
-
-@Transactional
-@Override
-public void deleteUser(String email) {
-    User theUser = getUser(email);
-    if (theUser != null){
-        userRepository.deleteByEmail(email);
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 
-}
+    @Transactional
+    @Override
+    public void deleteUser(String email) {
+        User theUser = getUser(email);
+        if (theUser != null) {
+            userRepository.deleteByEmail(email);
+        }
 
-@Override
-public User getUser(String email) {
-    return userRepository.findByEmail(email)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-}
-}
+    }
 
-
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
